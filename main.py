5003959bd7c07e0c882c0f8e7492349ecbe22b8f
Mikaela Matthews <mikaelamatthews@Mikaelas-MacBook-Air.local>
2026-05-25 15:51:57 -0500

    ammo is able to shoot

--- main.py
+++ main.py
@@ -60,21 +60,17 @@ async def main():
 		def __init__(self, x_pos, y_pos, width, height, color):
 			super().__init__(x_pos, y_pos, width, height, color)
 			self.rect = pygame.Rect((0, 0), (self.width, self.height))
-			self.rect.center = (x_pos, y_pos)
+			self.rect.center = (self.x_pos, self.y_pos)
 			self.speed = 5
 
 		def drawAmmo(self, surface):
 			pygame.draw.rect(surface, self.color, self.rect)
 		
-		def shootAmmo(self):
-			pass
-
 	# PLAYER
 	player = Player(10, 10, 20, 20, "black")
 
 	# AMMO
-
-	ammo = Ammo(player.rect.centerx, player.rect.centery, 10, 10, "red")
+	ammoList = []
 
 	# MAIN LOOP
 	running = True
@@ -84,15 +80,24 @@ async def main():
 			if event.type == pygame.QUIT:
 				running = False
 
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_SPACE:
+					ammoList.append(Ammo(player.rect.centerx, player.rect.centery, 10, 10, "red"))
+
 		screen.fill("white")
 
 		# Player Movement
 		player.movePlayer()
 
+		# Ammo Movement
+		for i in ammoList:
+			i.rect.x += i.speed
+
 		# DRAW OBJECTS
 		player.drawPlayer(screen)
 
-		ammo.drawAmmo(screen)
+		for i in ammoList:
+			i.drawAmmo(screen)
 
 		pygame.display.update()
 
